125bbcae9d2d299971da9d47f7afdfa10da441b9
D3ENNY <denysraimondi06@gmail.com>
2024-08-01 14:19:07 +0200

    formatted string

--- python/classi/main.py
+++ python/classi/main.py
@@ -45,13 +45,3 @@ if __name__ == '__main__': # ingresso principale programma
     classe_esterna = second.Second()
     classe_esterna.method()
     
-    # FORMATTED STRING
-    str1 = "ciao"
-    str2 = "Paolo"
-    
-    print()
-    print(str1 + " " + str2)
-    print(f"{str1} {str2}")
-    print("%s %s" % (str1, str2))
-    print("{} {}".format(str1, str2))
-    print(str1[::-1])
---
+++ python/stringhe/string.py
@@ -0,0 +1,10 @@
+# FORMATTED STRING
+str1 = "ciao"
+str2 = "Paolo"
+
+print()
+print(str1 + " " + str2)
+print(f"{str1} {str2}")
+print("%s %s" % (str1, str2))
+print("{} {}".format(str1, str2))
+print(str1[::-1])
