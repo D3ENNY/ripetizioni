a7707ce07ed87661b283f160d42274c290cea622
D3ENNY <denysraimondi06@gmail.com>
2024-08-02 15:35:20 +0200

    optimize matrici codebase

--- python/matrici/index.py
+++ python/matrici/index.py
@@ -5,38 +5,62 @@ matrice = [] # [[1,2,3], [1,2,3]]
 
 # METODI ARRAY 
 
+print("---SPIEGAZIONE ARRAY---")
 for i in range(0,10):
     arr.append(i) # push to array    
     
-# print(arr)
+print(arr)
+
+# FUNZIONI
+
+def stampa_matrice(matrice):
+    for i in range(0, len(matrice)):
+        print(matrice[i])
 
 # ESERCIZIO MATRICE
 
-n = input("inserire lato matrice: ")
-val = 0
+n = int(input("\ninserire lato matrice: "))
 
-for i in range(0, int(n)):
+print("\n----ESERCIZIO 1----")
+
+for i in range(0, n):
     matrice.append([])
     if i % 2 == 0:
-        for _ in range(0, int(n)):
+        for _ in range(0, n):
             matrice[i].append(1)
     else: 
-        for _ in range(0, int(n)):
+        for _ in range(0, n):
             matrice[i].append(0) 
             
         
-for i in range(0, len(matrice)):
-    print(matrice[i])
+stampa_matrice(matrice)
 
-print("############# GHE SBORO #############")
+print("\n############# GHE SBORO #############")
+
+print("\n---ESERCIZIO 1.2---")
+
+val = 0
+matrice = []
+
+for i in range(n):
+    matrice.append([])
+    val = val + 1 if i % 2 == 0 else val - 1
+
+    for _ in range(0, n):
+        matrice[i].append(val)
+        
+stampa_matrice(matrice) 
+
+print("\n############# GHE SBORO #############")
+
+print("\n---ESERCIZIO 2---")
 
 matrice = []
-i = 0
-for i in range(int(n)):
+for i in range(n):
     matrice.append([])
-    
+
     if i % 2 == 0: 
-        for _ in range(int(n)):
+        for _ in range(n):
             lenght = len(matrice[i])
             if lenght % 2 == 0:
                 matrice[i].append(0)
@@ -44,12 +68,29 @@ for i in range(int(n)):
                 matrice[i].append(1)
                 
     else: 
-        for _ in range(int(n)):
+        for _ in range(n):
             lenght = len(matrice[i])
             if lenght % 2 != 0:
                 matrice[i].append(0)
             else:
                 matrice[i].append(1)
             
-for i in range(0, len(matrice)):
-    print(matrice[i])
+stampa_matrice(matrice)
+
+print("\n############# GHE SBORO #############")
+
+print("\n---ESERCIZIO 2.2---")
+matrice = []
+
+for i in range(n):
+    matrice.append([0 for i in range(n)])
+    val = 0 if i % 2 == 0 else 1
+    for index in range(n):
+        matrice[i][index] = val
+        if val == 0: 
+            val = 1
+        else:
+            val = 0
+            
+
+stampa_matrice(matrice)
