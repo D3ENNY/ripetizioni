36bf9d6f65ba0e5b7e470730ba8cd17fd7ba4c7e
D3ENNY <denysraimondi06@gmail.com>
2024-08-04 12:45:57 +0200

    es10 cpp

---
+++ C++/Es10/es10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main() {
+    const int BASE_60 = 60; 
+    int hour, minutes, seconds, totSec;
+
+    std::cout << "Inserire ore: ";
+    std::cin >> hour;
+    std::cout << "Inserire minuti: ";
+    std::cin >> minutes;
+    std::cout << "Inserire secondi: ";
+    std::cin >> seconds;
+
+    // sappiamo che in un ora sono presenti 3600 secondi
+    // sappiamo che in un minuto sono presenti 60 secondi
+    totSec = (hour * BASE_60^2) + (minutes * BASE_60) + seconds;
+
+    std::cout << "il totale dei secondi sono: " << totSec << std::endl;
+}
