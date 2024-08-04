a78e15ce2df6829457e390a27f7017ab22910a3d
D3ENNY <denysraimondi06@gmail.com>
2024-08-04 11:40:32 +0200

    es6 cpp

---
+++ C++/Es6/es6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+    int a,b,c,avg;
+
+    std::cout << "inserire un numero:" << std::endl;
+    std::cin >> a;
+
+    std::cout << "inserire un numero:" << std::endl;
+    std::cin >> b;
+
+    std::cout << "inserire un numero:" << std::endl;
+    std::cin >> c;
+
+    avg = (a+b+c) / 3;
+
+    std::cout << "la media dei tre numeri è: " << avg << std::endl;
+
+}
