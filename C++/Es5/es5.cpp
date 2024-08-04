0098de0bb658cbf3aca7c12a2f9661975f259b3b
D3ENNY <denysraimondi06@gmail.com>
2024-08-04 11:40:28 +0200

    es5 cpp

---
+++ C++/Es5/es5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+int main(){
+    int a,b, even = 0, shots = 0;
+
+    std::cout << "inserire due numeri A e B con A < B\ninserire il numero A: ";
+    std::cin >> a;
+
+    do{
+        std::cout << "inserire il numero B: ";
+        std::cin >> b;
+    }while(a>b);
+
+    for (int i=a; i<=b; i++)
+        if (i % 2 == 0)
+            even++;
+        else 
+            shots++;
+    
+    if (even > shots)
+        std::cout << "nell'intervallo " << a << " - " << b << " ci sono più numeri pari";
+    else if (even == shots)
+         std::cout << "nell'intervallo " << a << " - " << b << " ci sono tanti numeri pari quanti i dispari";
+    else if (even < shots)
+        std::cout << "nell'intervallo " << a << " - " << b << " ci sono meno numeri pari";
+}
