6bf82bd6832cf8fca2fd74a0337b70ff8516a352
D3ENNY <denysraimondi06@gmail.com>
2024-08-04 11:52:32 +0200

    es 8 cpp

---
+++ C++/Es8/es8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main() {
+    int a, b;
+
+    std::cout << "inserire un numero:" << std::endl;
+    std::cin >> a;
+
+    std::cout << "inserire un numero:" << std::endl;
+    std::cin >> b;
+
+    if (a > b)
+        std::cout << "numeri in ordine crescente: " << b << " - " << a << std::endl;
+    else
+        std::cout << "numeri in ordine crescente: " << a << " - " << b << std::endl;
+}
